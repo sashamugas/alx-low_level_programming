344761c6428f368bdf5ad3fca0abb88616ce2005
sashamugas <richard.sasha116@gmail.com>
2022-10-14 23:04:07 +0300

    error handling

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,16 +9,10 @@ int main(void)
 	float decimalNumber;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(character));
-	printf("Size of a an int: %lu byte(s)\n", sizeof(interger));
+	printf("Size of a an int: %lu byte(s)\n", sizeof(integer));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longNumber));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(reallyLongNumber));
 	printf("Size of a float: %lu byte(s)\n", sizeof(decimalNumber));
-	
-	
-
-
-
-
 
 	return(0);
 }
