e3696245a942a6790b3b22d763f639285dbe6ff0
sashamugas <richard.sasha116@gmail.com>
2022-10-14 23:29:39 +0300

    doing everything to please betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main: entry point of program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
