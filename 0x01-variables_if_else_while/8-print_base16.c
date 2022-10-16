c20437bd6e4fe1cba7c2be1c48571c8ed87f50fc
sashamugas <richard.sasha116@gmail.com>
2022-10-17 01:08:18 +0300

    base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers in base 16
+ * main: Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char nums;
+
+	for (nums = '0'; nums <= '9'; nums++)
+	{
+		putchar(nums);
+	}
+
+	for (nums = 'a'; nums <= 'f'; nums++)
+	{
+		putchar(nums);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
