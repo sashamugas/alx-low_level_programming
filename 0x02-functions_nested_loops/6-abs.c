88f009b06faae4dce149a24074d7ca5b5489d30c
sashamugas <richard.sasha116@gmail.com>
2022-10-20 04:15:18 +0300

    absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolute value.
+ * _abs: gives the absolute value of an integer. makes the integer unsigned essentially.
+ * @n: input number as integer.
+ *
+ * Return: absolute value of n
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (n * -1);
+	_putchar('\n');
+}
