abfac498b874ade8bee432256502cda713a3ce9c
sashamugas <richard.sasha116@gmail.com>
2022-10-20 04:19:34 +0300

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _abs - absolute value.
- * _abs: gives the absolute value of an integer. 
+ * _abs: gives the absolute value of an integer.
  * @n: input number as integer.
  *
  * Return: absolute value of n
