231a33033bc18c795f8343f6626612f05b702d34
sashamugas <richard.sasha116@gmail.com>
2022-10-20 03:22:26 +0300

    print the alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,15 @@
 #include "main.h"
-
+/**
+ * print_alphabet_x10 - print the alphabet and a new line 10 times
+ *
+ * Return: Always void
+ */
 void print_alphabet_x10(void)
 {
 	int ch;
+
 	int i = 1;
+
 	while (i <= 10)
 	{
 		for (ch = 97; ch <= 122; ch++)
