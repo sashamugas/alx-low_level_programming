254b162ac0449c1286bd526e1252c61fed4c063e
sashamugas <richard.sasha116@gmail.com>
2022-10-21 23:22:41 +0300

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main- test if function prints negative or positive
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
