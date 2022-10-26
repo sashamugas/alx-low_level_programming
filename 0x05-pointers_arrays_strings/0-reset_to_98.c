6a603081b48754dc2708eaac985f8a5abf634c7f
sashamugas <richard.sasha116@gmail.com>
2022-10-27 02:09:29 +0300

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - dereference a pointer into an int
+ * @n: integer being dereferenced
+ * Return: (0)
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+pointers and how to use them
+arrays and how to use them
+differences betweeen pointers and arrays
+strings and how to manipulate them
+scope of variables
+
